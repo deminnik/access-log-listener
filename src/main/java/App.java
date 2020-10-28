6df73240b1f5a6dda8f09a598816622f4146ab40
deminnik <n.demin1@alumni.nsu.ru>
2020-10-28 16:23:59 +0700

    Работа по выводу результата

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,24 +1,21 @@
-import org.apache.commons.cli.*;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 import java.util.*;
-import java.util.regex.Pattern;
 
 public class App
 {
     public static void main(String[] args)
     {
         AppOptions options = new AppOptions(args);
-        AccessLogReader reader = new AccessLogReader();
 
+        AccessLogReader reader = new AccessLogReader();
         reader.readLog(options);
 
+        printResult(reader.getRecords());
+    }
+
+    private static void printResult(List<Record> records) {
         for (Record record :
-                reader.getRecords()) {
-            String str = String.format("%d.%d.%d - %d.%d.%d : %d",
+                records) {
+            String str = String.format("%d.%d.%d - %d.%d.%d : %f",
                     record.getStartTime().get(Calendar.HOUR),
                     record.getStartTime().get(Calendar.MINUTE),
                     record.getStartTime().get(Calendar.SECOND),
